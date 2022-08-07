9f15242da0ba021050ecdf893c3382b725715f90
sumairijazhashmi <sumairijazhashmi@gmail.com>
2022-08-07 13:06:14 +0500

    project files

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab, faSquareTumblr, faTumblr, faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faCheckSquare, faCoffee, faQuoteLeft } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faQuoteLeft, faTwitter, faTumblr, faSquareTumblr)
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
